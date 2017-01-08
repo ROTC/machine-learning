e9a9d0585552306d363433e03b8c3c267394b80a
liaolin <liaolin93@gmail.com>
2017-01-08 10:59:06 +0800

    矩阵和向量分别为大小写字母

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -2,10 +2,10 @@
 """
 程序说明：
 fit函数
-基于X和Y作线性回归拟合，求出系数coef_，shape为(1, _)
+基于X和y作线性回归拟合，求出系数coef_，shape为(1, _)，X为矩阵，y为向量
 
 predict函数
-基于coef_进行预测，预测值的shape为(1, _)
+基于coef_对X进行预测，预测值的shape为(1, _)，X为矩阵
 
 程序验证部分参考sklearn中的Linear Regression Example
 """
@@ -18,17 +18,17 @@ class LinearRegressor:
     def __init__(self):
         self.coef_ = None
 
-    def fit(self, X, Y):
+    def fit(self, X, y):
         X = np.array(X).reshape((len(X), -1))
         X = np.c_[np.ones(len(X)), X]   #add a constant column
-        Y = np.array(Y).reshape((len(Y), -1))
-        if X.shape[0] != Y.shape[0] or Y.shape[1] != 1:
+        y = np.array(y).reshape((len(y), -1))
+        if X.shape[0] != y.shape[0] or y.shape[1] != 1:
             raise Exception('Input Error')
-        self.coef_= np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)[:, 0]
+        self.cofe_= np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)[:, 0]
 
-    def predict(self, x):
-        x = np.c_[np.ones(len(x)), np.array(x)]
-        y = self.coef_.dot(x.T)
+    def predict(self, X):
+        X = np.c_[np.ones(len(X)), np.array(X)]
+        y = self.coef_.dot(X.T)
         return y
 
 if __name__ == '__main__':
@@ -37,13 +37,13 @@ if __name__ == '__main__':
 
     diabetes_X_train = diabetes_X[:-20]
     diabetes_X_test = diabetes_X[-20:]
-    diabetes_Y_train = diabetes.target[:-20]
-    diabetes_Y_test = diabetes.target[-20:]
+    diabetes_y_train = diabetes.target[:-20]
+    diabetes_y_test = diabetes.target[-20:]
 
     regr = LinearRegressor()
-    regr.fit(diabetes_X_train, diabetes_Y_train)
+    regr.fit(diabetes_X_train, diabetes_y_train)
 
     #The coefficients
     print('Coefficients: \n', regr.coef_)
     #The mean squared error
-    print('Mean squared error: %.2f' % np.mean((regr.predict(diabetes_X_test)-diabetes_Y_test)**2))
+    print('Mean squared error: %.2f' % np.mean((regr.predict(diabetes_X_test)-diabetes_y_test)**2))
